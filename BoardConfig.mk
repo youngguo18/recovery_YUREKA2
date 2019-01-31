0308aad6423f3e3e5773878829e60ac55177bcb5
dianlujitao <dianlujitao@lineageos.org>
2019-01-31 22:26:17 +0800

    YUREKA2:Use libcryptfs_hw from commonsys repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,7 @@ TARGET_BOOTLOADER_BOARD_NAME := msm8953
 TARGET_NO_BOOTLOADER := true
 
 # Crypto
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Kernel
